3fbc63878942a0d87a9345fb7d55e04ed0f2c782
Chris Arceneaux <carcenea@gmail.com>
2020-09-14 16:29:29 -0400

    Initial working version
    - Improved verbose message to be more helpful
    - Fixed tenant name for VCD backup jobs
      - Previously tenant name was VBR server name
    - Added the following parameter to the initial data collection
      - Added repository information
      - Added parent/child backup information
    - Added logic to determine...
      - GFS/non-GFS backups
      - Active/Inactive backups
        - Different logic required for forward vs reverse incremental
    - Combined parent/child backups prior to applying logic
      - This was necessary as, without it, it was impossible to determine active/inactive chains
    - Completed generation of final output
      - Output consists of the following line items per repo:
        - Repo name
        - Is SOBR?
        - GFS Size GB
        - Non-GFS Size GB
      - Output is a PSObject
    
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- VCC-CapacityTierCalc/Get-CapacityTierCalc.ps1
+++ VCC-CapacityTierCalc/Get-CapacityTierCalc.ps1
@@ -28,11 +28,16 @@ if ($registered -eq $false) { Add-PSSnapin VeeamPSSnapin }
 
 # Creating empty ArrayList
 $aResults = [System.Collections.ArrayList]::new()
+$repoList = [System.Collections.ArrayList]::new()
 
 ##### Data collection begins here #####
+Write-Verbose "################################"
+Write-Verbose "Starting data collection..."
 
-# Retrieving all Cloud Connect Tenants
+# Retrieving Cloud Connect Information
 $tenants = Get-VBRCloudTenant
+$repoList.Add((Get-VBRBackupRepository)) | Out-Null
+$repoList.Add((Get-VBRBackupRepository -ScaleOut)) | Out-Null
 
 # Looping through tenants
 foreach ($tenant in $tenants) {
@@ -47,7 +52,7 @@ foreach ($tenant in $tenants) {
             continue
         }
 
-        Write-Verbose "Analyzing tenant backups located on $($repo.name)"
+        Write-Verbose "Retrieving tenant backups located on $($repo.name)"
         $backups = $repo.GetBackups() #undocumented API call
 
         # All backups for all tenants at least on this repo
@@ -56,6 +61,11 @@ foreach ($tenant in $tenants) {
 
             # Root element is tenant name
             $name = ($backup.GetFullPathWithTenantFolder()).Elements[0]
+            # Below conditional accounts for some backups jobs have the Veeam server as the first element
+            if ($name -notmatch ($tenants.name -join '|')) {
+                $name = ($backup.GetFullPathWithTenantFolder()).Elements[1]
+            }
+
             # retrieves all files related to backup
             $files = $backup.GetAllStorages()
             $files += $backup.GetAllChildrenStorages()
@@ -81,17 +91,31 @@ foreach ($tenant in $tenants) {
                 # Convert BackupSize from KB to GB
                 $backupSizegb = [math]::Round($backupSizekb / 1gb, 2)
 
+                # Retrieve repository information
+                $repoName = $repo.Name
+                $type = ($repoList | Where-Object {$_.Id.Guid -eq $backup.RepositoryId.Guid}).GetType().Name
+                if ("VBRScaleOutBackupRepository" -eq $type) {
+                    $sobr = $true
+                } else {
+                    $sobr = $false
+                }
+
                 $fileDetail = [PSCustomObject]@{
-                    Id           = $file.Id
-                    VmId         = $vmID
-                    RepoId       = $backup.RepositoryId.Guid
-                    Tenant       = $name
-                    FileName     = $file.FilePath.ToString()
-                    BackupType   = $BackupType
-                    BackupSizeGB = $backupSizegb
-                    CreationTime = $file.CreationTime
-                    GfsPeriod    = $file.GfsPeriod
-                    JobName      = $backup.JobName
+                    Id             = $file.Id.Guid
+                    VmId           = $vmID
+                    RepoId         = $backup.RepositoryId.Guid
+                    RepoName       = $repoName
+                    SOBR           = $sobr
+                    Tenant         = $name
+                    FileName       = $file.FilePath.ToString()
+                    BackupType     = $BackupType
+                    BackupSizeGB   = $backupSizegb
+                    CreationTime   = $file.CreationTime
+                    GfsPeriod      = $file.GfsPeriod
+                    JobName        = $backup.JobName
+                    BackupId       = $backup.Id.Guid
+                    IsChildBackup  = $backup.IsChildBackup
+                    ParentBackupId = $backup.ParentBackupId.Guid
                 }
                 # Add data to ArrayList
                 $aResults.Add($fileDetail) | Out-Null
@@ -104,9 +128,133 @@ foreach ($tenant in $tenants) {
 $aResults = $aResults | Sort-Object -Property Id -Unique
 
 ##### Parsing data & applying logic for final report #####
+Write-Verbose "################################"
+Write-Verbose "Data collection complete: Applying logic for final report"
 
+# Get current time
+$current = Get-Date
+# Creating empty ArrayList
+$offload = [System.Collections.ArrayList]::new()
 
+# Determine if GFS or not
+Write-Verbose "Sorting backups GFS vs non-GFS..."
+$gfs = $aResults | Where-Object { $_.GfsPeriod -ne "None" }
+$notGfs = $aResults | Where-Object { $_.GfsPeriod -eq "None" }
+Write-Verbose "GFS: $($gfs.count) - Non-GFS: $($notGfs.count) - Total: $($aResults.count)"
 
+### GFS
+# Is older than $Days?
+$offload_gfs = $gfs | Where-Object { (New-TimeSpan $_.CreationTime $current).TotalDays -gt $Days }
+foreach ($item in $offload_gfs) {
+    $offload.Add($item) | Out-Null
+}
 
+### NOT GFS
+# Matching Parent/Child JobNames - makes sure backup chains are complete
+for ($i = 0; $i -le $notGfs.count; $i++) {
+    if ($notGfs[$i].IsChildBackup) {
+        # Child backup found - Searching for parent
+        $name = ($notGfs | Where-Object { $_.BackupId -eq ($notGfs[$i].ParentBackupId) }).JobName
+        # Parent backup found - Updating JobName to match parent
+        $notGfs[$i].JobName = $name
+    }
+}
 
-return $aResults
+# Looping through tenants
+$tenants = ($notGfs | Select-Object Tenant -Unique).Tenant
+foreach ($tenant in $tenants) {
+    Write-Verbose "--------------------------------"
+
+    # Looping through tenant backup jobs
+    $jobs = ($notGfs | Where-Object { $_.Tenant -eq $tenant } | Select-Object JobName -Unique).JobName
+    Write-Verbose "$($tenant): Analyzing $($jobs.count) backup jobs"
+    foreach ($job in $jobs) {
+        # Initializing variable
+        $inactive = $null
+
+        # Identifying all files belonging to job
+        $files = $notGfs | Where-Object { ($_.Tenant -eq $tenant) -and ($_.JobName -eq $job) }
+        # Finding latest full backup for job
+        $latest = $files | Sort-Object CreationTime -Descending | Select-Object -First 1
+
+        # Determine inactive chains - different logic depending on job backup mode
+        # https://helpcenter.veeam.com/docs/backup/vsphere/capacity_tier_inactive_backup_chain.html?ver=100
+        if ($files.FileName -match ".vib") {
+            # Backup Mode: Forward Incremental
+            # Finding all backups previous to the most recent full backup
+            $inactive = ($files | Where-Object { (New-TimeSpan $_.CreationTime $latest.CreationTime).TotalSeconds -gt 0 })
+        }
+        elseif ($files.FileName -match ".vrb") {
+            # Backup Mode: Reverse Incremental
+            # Finding all backups previous to the most recent full backup
+            $files = $files | Where-Object { (New-TimeSpan $_.CreationTime $latest.CreationTime).TotalSeconds -le 0 } | Sort-Object CreationTime -Descending  #sort newest to oldest
+            # Finding where inactive chain begins
+            switch ($true) {
+                ("Full" -eq $files[0].BackupType) {
+                    #inactive chain begins on this full backup
+                    $inactive = $files
+                    break
+                }
+                ("Full" -eq $files[1].BackupType) {
+                    #inactive chain begins on this full backup
+                    $inactive = $files | Where-Object { $_.Id -ne $files[0].Id }
+                    break
+                }
+                default {
+                    #inactive chain begins after most recent full backup and 2 incrementals
+                    $inactive = $files | Where-Object { ($_.Id -ne $files[0].Id) -and ($_.Id -ne $files[1].Id) }
+                    break
+                }
+            }
+        }
+        else {
+            # Jobs with only full backups: Backup Mode doesn't matter
+            $inactive = $files
+        }
+
+        # Which files of inactive chain(s) are older than $Days?
+        $offload_nonGfs = $inactive | Where-Object { (New-TimeSpan $_.CreationTime $current).TotalDays -gt $Days }
+        if ($offload_nonGfs) {
+            foreach ($item in $offload_nonGfs) {
+                $offload.Add($item) | Out-Null
+            }
+        }
+    }
+}
+
+# Sample output format
+#Repository | SOBR | GFS | Sealed | Unsealed | CapacityTierGB
+
+# Creating empty ArrayList
+$output = [System.Collections.ArrayList]::new()
+
+# Looping through repositories
+$repos = ($offload | Select-Object RepoName -Unique).RepoName
+Write-Verbose "###### Backups to be offloaded ######"
+foreach ($repo in $repos) {
+    $sobr = ($offload | Where-Object { $_.RepoName -eq $repo } | Select-Object -First 1).SOBR
+    $gfsCount = @($offload | Where-Object { ($_.RepoName -eq $repo) -and ($_.GfsPeriod -ne "None") }).count
+    $nonGfsCount = @($offload | Where-Object { ($_.RepoName -eq $repo) -and ($_.GfsPeriod -eq "None") }).count
+
+    Write-Verbose "Repository: $repo"
+    Write-Verbose "GFS: $gfsCount"
+    Write-Verbose "Non-GFS: $nonGfsCount"
+
+    # Summing up totals
+    $gfsSizeGb = ($offload | Where-Object { ($_.RepoName -eq $repo ) -and ($_.GfsPeriod -ne "None") } | Measure-Object BackupSizeGB -Sum).Sum
+    $nonGfsSizeGb = ($offload | Where-Object { ($_.RepoName -eq $repo) -and ($_.GfsPeriod -eq "None") } | Measure-Object BackupSizeGB -Sum).Sum
+
+    $row = [PSCustomObject]@{
+        Repository   = $repo
+        SOBR         = $sobr
+        GfsSizeGB    = if ($gfsSizeGb){ $gfsSizeGb } else { 0 }
+        NonGfsSizeGb = if ($nonGfsSizeGb){ $nonGfsSizeGb } else { 0 }
+    }
+    # Add data to output
+    $output.Add($row) | Out-Null
+}
+Write-Verbose "Total amount to be offloaded: $(($offload | Measure-Object BackupSizeGB -Sum).Sum)GB"
+Write-Verbose "################################"
+Write-Warning "Output values are estimates and will vary depending on the data being backed up. GFS backups can achieve the greatest block clone space savings up to/and over 90%. This tool can only provide estimates and not hard numbers."
+
+return $output
